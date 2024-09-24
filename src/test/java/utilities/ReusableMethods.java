cf0d40d96c0c388e49dd822d24867423490c1809
cmbakir <cembakir@hotmail.com>
2024-09-24 23:01:31 +0300

    day14 gunsonu

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,9 +1,12 @@
-
 package utilities;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -62,5 +65,100 @@ public class ReusableMethods {
             }
 
         }
+    }
+
+    public static void getScreenshotTumSayfa(WebDriver driver,String ssIsmi){
+        // 1.adim olusturacagimiz dosyayolunu hazirlayalim
+        String dosyaYolu = "target/screenshots/"+ssIsmi+".png";
+
+        // 2. TakesScreenshot objesi olusturalim
+
+        // TakesScreenshot tss = new TakesScreenshot();
+        // 'TakesScreenshot' is abstract; cannot be instantiated
+
+        TakesScreenshot tss = (TakesScreenshot) driver;
+
+        // 3.adim screenshot'i kaydedecegimiz File'i olusturalim
+
+        File tumsayfaSS = new File(dosyaYolu);
+
+        // 4.adim screenshot'i alip gecici dosua olarak kaydedin
+
+        File geciciResim = tss.getScreenshotAs(OutputType.FILE);
+
+        // 5.adim gecici resmi asil dosyaya kopyalayalim
+
+        try {
+            FileUtils.copyFile(geciciResim,tumsayfaSS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void getScreenshotTumSayfa(WebDriver driver){
+
+        // 240924202034
+        LocalDateTime ldt = LocalDateTime.now(); // 2024-09-24T20-23-24-123432
+
+        DateTimeFormatter istenenFormat = DateTimeFormatter.ofPattern("yyMMddHHmmss");
+        String tarihEtiketi = ldt.format(istenenFormat);
+
+        // 1.adim olusturacagimiz dosyayolunu hazirlayalim
+        String dosyaYolu = "target/screenshots/TumSayfaSS"+tarihEtiketi+".png";
+
+        // 2. TakesScreenshot objesi olusturalim
+
+        // TakesScreenshot tss = new TakesScreenshot();
+        // 'TakesScreenshot' is abstract; cannot be instantiated
+
+        TakesScreenshot tss = (TakesScreenshot) driver;
+
+        // 3.adim screenshot'i kaydedecegimiz File'i olusturalim
+
+        File tumsayfaSS = new File(dosyaYolu);
+
+        // 4.adim screenshot'i alip gecici dosua olarak kaydedin
+
+        File geciciResim = tss.getScreenshotAs(OutputType.FILE);
+
+        // 5.adim gecici resmi asil dosyaya kopyalayalim
+
+        try {
+            FileUtils.copyFile(geciciResim,tumsayfaSS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void getScreenshotWebElement(WebDriver driver, WebElement webElement){
+
+        LocalDateTime ldt = LocalDateTime.now(); // 2024-09-24T20-23-24-123432
+
+        DateTimeFormatter istenenFormat = DateTimeFormatter.ofPattern("yyMMddHHmmss");
+        String tarihEtiketi = ldt.format(istenenFormat);
+
+        // 1.adim olusturacagimiz dosyayolunu hazirlayalim
+        String dosyaYolu = "target/screenshots/WebElementSS"+tarihEtiketi+".png";
+
+        // 2- kullanacagimiz WebElementi locate edip, WebElement olarak kaydedin
+        //    parametre olarak gonderilen webElement'in ss alinacak
+
+        // 3- kaydedecegimiz file'i olusturun
+
+        File webElementSS = new File(dosyaYolu);
+
+        // 4- webElement uzerinden screenshot'i alip, gecici dosya olarak kaydedin
+
+        File geciciDosya = webElement.getScreenshotAs(OutputType.FILE);
+
+        // 5- gecici dosyayi asil dosyaya kopyala
+
+        try {
+            FileUtils.copyFile(geciciDosya,webElementSS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
